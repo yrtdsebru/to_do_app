7e7367f36a9b3f778991bca7fceba654578b6c94
Ebru <ebru.yrtds@gmail.com>
2023-12-16 02:32:24 +0300

    update 🆕: Localization added, Page Route changed, super key

--- lib/app/views/view_splash/splash_view.dart
+++ lib/app/views/view_splash/splash_view.dart
@@ -2,26 +2,27 @@ import 'dart:async';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_app/app/l10n/app_localizations.dart';
 import 'package:to_do_app/app/routes/app_router.gr.dart';
 import 'package:to_do_app/core/constants/app_colors.dart';
 
 @RoutePage()
 class SplashView extends StatelessWidget {
-  const SplashView({Key? key}) : super(key: key);
+  const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
     Timer(
         const Duration(seconds: 5),
         () => AutoRouter.of(context)
-            .replace(const HomeViewRoute()));
+            .replace(const RegistrationViewRoute()));
             
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: Center(
           child: Text(
-        "Catty ᓚᘏᗢ",
-        style: TextStyle(
+        L10n.of(context)!.splashLogo,
+        style: const TextStyle(
           fontSize: 50,
           fontWeight: FontWeight.w900,
           color: AppColors.white,
