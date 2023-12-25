3660f1d86bee07863bfceb8022fd76059487a1a5
Ebru <ebru.yrtds@gmail.com>
2023-12-25 23:49:06 +0300

    new features

--- lib/app/views/view_home/home_view.dart
+++ lib/app/views/view_home/home_view.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:to_do_app/app/l10n/app_localizations.dart';
 import 'package:to_do_app/app/routes/app_router.gr.dart';
+import 'package:to_do_app/core/constants/config.dart';
 import 'package:to_do_app/core/theme/theme.dart';
+import 'package:to_do_app/core/widgets/bottom_sheet_widget.dart';
+import 'package:http/http.dart' as http;
 
 @RoutePage()
 class HomeView extends StatefulWidget {
@@ -13,7 +20,49 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  late String userId;
   final TextEditingController searchController = TextEditingController();
+  List? taskList;
+  var isSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    openBox();
+  }
+
+  void openBox() async {
+    var box = await Hive.openBox('settings');
+    var myToken = box.get('token');
+    Map<String, dynamic> JwtDecodedToken =
+        JwtDecoder.decode(myToken); // tokeni decode etti
+    userId = JwtDecodedToken[
+        '_id']; // tokendan userId'yi aldik, userController'da token içeriği var, login yaparken tokeni olusturduk ya (tokenData = { _id: user._id, email: user.email };)
+    getTaskList(userId);
+  }
+
+  void isTaskSelected() {
+    setState(() {
+      isSelected = !isSelected;
+    });
+  }
+
+  void searchFunction(String query) async {}
+
+  void getTaskList(userId) async {
+    var taskBody = {
+      "userId": userId,
+    };
+
+    var response = await http.post(Uri.parse(getTask),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(taskBody));
+
+    var jsonResponse = jsonDecode(response.body);
+    taskList = jsonResponse['success'];
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +76,7 @@ class _HomeViewState extends State<HomeView> {
               alignment: Alignment.topRight,
               child: IconButton(
                 onPressed: () {
-                  // context.router.push(const SignInViewRoute());
+                  CustomButtomSheet(context);
                 },
                 icon: Icon(Icons.menu,
                     color: Themes.buttonColor.withOpacity(0.8), size: 35),
@@ -49,7 +98,7 @@ class _HomeViewState extends State<HomeView> {
                     const SizedBox(height: 20),
                     Container(
                       decoration: BoxDecoration(
-                        color: Colors.transparent.withOpacity(0.2),
+                        color: Colors.transparent.withOpacity(0.05),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: TextField(
@@ -69,9 +118,68 @@ class _HomeViewState extends State<HomeView> {
                       ),
                     ),
                     const SizedBox(height: 20),
-                    const Center(
-                      child: Text("Home"),
-                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: taskList == null
+                          ? Container(
+                              child: Center(
+                                child: Text(L10n.of(context)!.password,
+                                    style: TextStyle(
+                                        color:
+                                            Themes.textColor.withOpacity(0.6))),
+                              ),
+                            )
+                          : Expanded(
+                              child: ListView.builder(
+                                  itemCount: taskList!.length,
+                                  itemBuilder: (context, index) {
+                                    return ListTile(
+                                      title: Text(taskList![index]['title'],
+                                          style: TextStyle(
+                                              decoration: isSelected
+                                                  ? TextDecoration.lineThrough
+                                                  : TextDecoration.none,
+                                              color: Themes.textColor,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold)),
+                                      subtitle: Text(taskList![index]['desc'],
+                                          style: TextStyle(
+                                              decoration: isSelected
+                                                  ? TextDecoration.lineThrough
+                                                  : TextDecoration.none,
+                                              color: Themes.textColor
+                                                  .withOpacity(0.5),
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold)),
+                                      trailing: GestureDetector(
+                                        onTap: () {
+                                          isTaskSelected();
+                                        },
+                                        child: Container(
+                                          width: 30,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(100),
+                                            border: Border.all(
+                                                color: Themes.buttonColor
+                                                    .withOpacity(0.8),
+                                                width: 1.5),
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(5),
+                                            child: isSelected
+                                                ? Icon(Icons.check,
+                                                    color: Themes.buttonColor,
+                                                    size: 15)
+                                                : const SizedBox(),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            ),
+                    )
                   ],
                 ),
               ),
@@ -81,15 +189,16 @@ class _HomeViewState extends State<HomeView> {
         Container(
           width: double.infinity,
           height: 60,
-          decoration: BoxDecoration(color: Colors.transparent.withOpacity(0.2)),
+          decoration:
+              BoxDecoration(color: Colors.transparent.withOpacity(0.05)),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(),
               Text(
-                "           " + "59 Not",
+                "           " + "59" +" Not",
                 style: TextStyle(
-                  color: Themes.bgTextColor,
+                  color: Themes.textColor,
                   fontSize: 14,
                 ),
               ),
@@ -98,7 +207,7 @@ class _HomeViewState extends State<HomeView> {
                   context.router.push(const TaskViewRoute());
                 },
                 icon: Icon(Icons.note_add_rounded,
-                    color: Themes.buttonColor.withOpacity(0.8), size: 35),
+                    color: Themes.buttonColor, size: 35),
               ),
             ],
           ),
--- lib/app/views/view_task/task_view.dart
+++ lib/app/views/view_task/task_view.dart
@@ -1,8 +1,14 @@
+// ignore_for_file: use_build_context_synchronously
+import 'dart:convert';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:intl/intl.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:to_do_app/app/l10n/app_localizations.dart';
 import 'package:to_do_app/core/theme/theme.dart';
+import 'package:http/http.dart' as http;
+import 'package:to_do_app/core/constants/config.dart';
 
 @RoutePage()
 class TaskView extends StatefulWidget {
@@ -13,11 +19,59 @@ class TaskView extends StatefulWidget {
 }
 
 class _TaskViewState extends State<TaskView> {
+  late String userId;
   final TextEditingController title = TextEditingController();
   final TextEditingController description = TextEditingController();
 
+
   String currentDateTime =
       DateFormat('dd MMMM yyyy HH:mm').format(DateTime.now());
+
+  @override
+  void initState() {
+    super.initState();
+    openBox();
+  }
+
+  void openBox() async {
+    var box = await Hive.openBox('settings');
+    var myToken = box.get('token');
+    Map<String, dynamic> JwtDecodedToken = JwtDecoder.decode(myToken); // tokeni decode etti
+    userId = JwtDecodedToken['_id']; // tokendan userId'yi aldik, userController'da token içeriği var, login yaparken tokeni olusturduk ya (tokenData = { _id: user._id, email: user.email };)
+    setState(() {});
+  }
+  
+  void addTask() async {
+    if (title.text.isNotEmpty || description.text.isNotEmpty) {
+      print("Title: ${title.text}");
+      print("Description: ${description.text}");
+
+      var taskBody = {  // to do controllerda const { userId,title, desc } = req.body;
+        "userId": userId,
+        "title": title.text,
+        "desc": description.text
+        // "date": currentDateTime
+      };
+
+      var response = await http.post(Uri.parse(createTask),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(taskBody));
+
+      var jsonResponse = jsonDecode(response.body);
+
+      print(jsonResponse['status']);
+
+      if (jsonResponse['status']) {
+        title.clear();
+        description.clear();
+        context.router.pop();
+      } else {
+        print("Something went wrong");
+      }
+    } else {
+      print("Please fill fields");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +90,13 @@ class _TaskViewState extends State<TaskView> {
             ),
             TextButton(
               onPressed: () {
-                // context.router.push(const SignInViewRoute());
+                addTask();
               },
               child: Text(L10n.of(context)!.done,
-                  style: TextStyle(color: Themes.buttonColor.withOpacity(0.8), fontSize: 15, fontWeight: FontWeight.bold)),
+                  style: TextStyle(
+                      color: Themes.buttonColor.withOpacity(0.8),
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold)),
             ),
           ],
         ),
@@ -51,14 +108,16 @@ class _TaskViewState extends State<TaskView> {
               children: [
                 Center(
                   child: Text(currentDateTime,
-                      style: TextStyle(color: Themes.textColor.withOpacity(0.6))),
+                      style:
+                          TextStyle(color: Themes.textColor.withOpacity(0.6))),
                 ),
                 TextField(
                   controller: title,
                   maxLines: 1,
                   decoration: InputDecoration(
                     hintText: L10n.of(context)!.title,
-                    hintStyle: TextStyle(color: Themes.textColor.withOpacity(0.7)),
+                    hintStyle:
+                        TextStyle(color: Themes.textColor.withOpacity(0.7)),
                     border: InputBorder.none,
                   ),
                   style: TextStyle(
@@ -70,7 +129,8 @@ class _TaskViewState extends State<TaskView> {
                   controller: description,
                   decoration: InputDecoration(
                     hintText: L10n.of(context)!.description,
-                    hintStyle: TextStyle(color: Themes.textColor.withOpacity(0.7)),
+                    hintStyle:
+                        TextStyle(color: Themes.textColor.withOpacity(0.7)),
                     border: InputBorder.none,
                   ),
                   style: TextStyle(color: Themes.textColor, fontSize: 25),
