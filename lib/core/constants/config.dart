fb8e0b1e97b92815e0d9987ef7f6f39414d3ef36
Ebru <ebru.yrtds@gmail.com>
2023-12-25 23:48:18 +0300

    add✨:new path

--- lib/core/constants/config.dart
+++ lib/core/constants/config.dart
@@ -1,3 +1,6 @@
-final url = "http://localhost:3000/";  //dogru adresi yazmak onemli.
-final registration = url + "register";
-final login = url + "login";
+const url = "http://localhost:3000/";  //dogru adresi yazmak onemli.
+const registration = url + "register";
+const login = url + "login";
+const createTask = url + "createToDo";
+const getTask = url + "getUserTodoList";
+const updateTask = url + "deleteTodo";
