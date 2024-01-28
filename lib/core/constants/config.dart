09a2866e77d60372ae5127e73b56aa5f0df12160
Ebru <ebru.yrtds@gmail.com>
2024-01-28 19:16:47 +0300

    update: deleteTask constant in config.dart

--- lib/core/constants/config.dart
+++ lib/core/constants/config.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 const url = "http://localhost:3000/";  //dogru adresi yazmak onemli.
 const registration = url + "register";
 const login = url + "login";
 const createTask = url + "createToDo";
 const getTask = url + "getUserTodoList";
-const updateTask = url + "deleteTodo";
+const deleteTask = url + "deleteTodo";
