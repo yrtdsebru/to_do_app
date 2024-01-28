0a5575b04f5d8e4e95a05ac9335003494eb24cb3
Ebru <ebru.yrtds@gmail.com>
2024-01-28 19:15:52 +0300

    fix: router method and update view files

--- lib/app/routes/app_router.gr.dart
+++ lib/app/routes/app_router.gr.dart
@@ -8,70 +8,56 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i7;
-import 'package:to_do_app/app/views/view_home/home_view.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i6;
+import 'package:to_do_app/app/views/view_home/home_view.dart' as _i1;
 import 'package:to_do_app/app/views/view_registration/registration_view.dart'
-    as _i3;
-import 'package:to_do_app/app/views/view_signin/signin_view.dart' as _i4;
-import 'package:to_do_app/app/views/view_splash/splash_view.dart' as _i5;
-import 'package:to_do_app/app/views/view_task/task_view.dart' as _i6;
+    as _i2;
+import 'package:to_do_app/app/views/view_signin/signin_view.dart' as _i3;
+import 'package:to_do_app/app/views/view_splash/splash_view.dart' as _i4;
+import 'package:to_do_app/app/views/view_task/task_view.dart' as _i5;
 
-abstract class $AppRouter extends _i7.RootStackRouter {
+abstract class $AppRouter extends _i6.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i7.PageFactory> pagesMap = {
+  final Map<String, _i6.PageFactory> pagesMap = {
     HomeViewRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i2.HomeView(),
+        child: const _i1.HomeView(),
       );
     },
     RegistrationViewRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i3.RegistrationView(),
+        child: const _i2.RegistrationView(),
       );
     },
     SignInViewRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.SignInView(),
+        child: const _i3.SignInView(),
       );
     },
     SplashViewRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.SplashView(),
+        child: const _i4.SplashView(),
       );
     },
     TaskViewRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i6.TaskView(),
+        child: const _i5.TaskView(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.DetailView]
-class DetailViewRoute extends _i7.PageRouteInfo<void> {
-  const DetailViewRoute({List<_i7.PageRouteInfo>? children})
-      : super(
-          DetailViewRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'DetailViewRoute';
-
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
-}
-
-/// generated route for
-/// [_i2.HomeView]
-class HomeViewRoute extends _i7.PageRouteInfo<void> {
-  const HomeViewRoute({List<_i7.PageRouteInfo>? children})
+/// [_i1.HomeView]
+class HomeViewRoute extends _i6.PageRouteInfo<void> {
+  const HomeViewRoute({List<_i6.PageRouteInfo>? children})
       : super(
           HomeViewRoute.name,
           initialChildren: children,
@@ -79,13 +65,13 @@ class HomeViewRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'HomeViewRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i3.RegistrationView]
-class RegistrationViewRoute extends _i7.PageRouteInfo<void> {
-  const RegistrationViewRoute({List<_i7.PageRouteInfo>? children})
+/// [_i2.RegistrationView]
+class RegistrationViewRoute extends _i6.PageRouteInfo<void> {
+  const RegistrationViewRoute({List<_i6.PageRouteInfo>? children})
       : super(
           RegistrationViewRoute.name,
           initialChildren: children,
@@ -93,13 +79,13 @@ class RegistrationViewRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'RegistrationViewRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i4.SignInView]
-class SignInViewRoute extends _i7.PageRouteInfo<void> {
-  const SignInViewRoute({List<_i7.PageRouteInfo>? children})
+/// [_i3.SignInView]
+class SignInViewRoute extends _i6.PageRouteInfo<void> {
+  const SignInViewRoute({List<_i6.PageRouteInfo>? children})
       : super(
           SignInViewRoute.name,
           initialChildren: children,
@@ -107,13 +93,13 @@ class SignInViewRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'SignInViewRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i5.SplashView]
-class SplashViewRoute extends _i7.PageRouteInfo<void> {
-  const SplashViewRoute({List<_i7.PageRouteInfo>? children})
+/// [_i4.SplashView]
+class SplashViewRoute extends _i6.PageRouteInfo<void> {
+  const SplashViewRoute({List<_i6.PageRouteInfo>? children})
       : super(
           SplashViewRoute.name,
           initialChildren: children,
@@ -121,13 +107,13 @@ class SplashViewRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'SplashViewRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i6.TaskView]
-class TaskViewRoute extends _i7.PageRouteInfo<void> {
-  const TaskViewRoute({List<_i7.PageRouteInfo>? children})
+/// [_i5.TaskView]
+class TaskViewRoute extends _i6.PageRouteInfo<void> {
+  const TaskViewRoute({List<_i6.PageRouteInfo>? children})
       : super(
           TaskViewRoute.name,
           initialChildren: children,
@@ -135,5 +121,5 @@ class TaskViewRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'TaskViewRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
